bedb6df91de3435511ba966f9f65b2759ee00ba4
brcastel <caso@caso-brian-u>
2016-09-28 11:49:31 -0700

    added support for centos jenkins build

--- test/scripts/deploy.sh
+++ test/scripts/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-hostname
-pwd
-cp ./test/files/build.yml ./roles/reset-build/files/
-cp ./test/files/test.yml .
+ansible-playbook ./test/playbooks/setup.yml
 ansible-playbook reset_build.yml
 ansible-playbook build.yml
 ./metro-ansible test.yml -vvvv
