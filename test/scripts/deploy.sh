c2e55892a7287ee19fd9f93760ce8b04320d182e
brcastel <caso@caso-brian-u>
2016-09-28 12:05:39 -0700

    fixed a setup file path problem

--- test/scripts/deploy.sh
+++ test/scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-ansible-playbook ./test/playbooks/setup.yml
+cp ./test/files/setup.yml .
+ansible-playbook setup.yml
 ansible-playbook reset_build.yml
 ansible-playbook build.yml
 ./metro-ansible test.yml -vvvv
