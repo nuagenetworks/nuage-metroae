b2d52722dd20a141b5542696a24f19735adfd7c9
shreya2602 <shreya.pagedar@gmail.com>
2016-11-05 15:14:20 -0400

    Script to test VSD only deployment

---
+++ test/scripts/vsd_standalone_deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp ./test/files/setup.yml.VSDStandalone ./setup.yml
+ansible-playbook setup.yml
+ansible-playbook reset_build.yml
+ansible-playbook build.yml
+./metro-ansible test.yml -vvvv
