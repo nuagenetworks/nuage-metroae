6aa3f66661f1a97b7b12be7d0114400e6c7a3f5b
spamulap12 <santhan.pamulapati@nokia.com>
2016-12-08 23:05:12 +0000

    adding vsd ram var

--- test/scripts/deploy_ci.sh
+++ test/scripts/deploy_ci.sh
@@ -7,4 +7,4 @@ ansible-playbook build.yml -vvvv
 ./metro-ansible ci_deploy.yml -vvvv
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
-#./metro-ansible test.yml
+./metro-ansible test.yml
