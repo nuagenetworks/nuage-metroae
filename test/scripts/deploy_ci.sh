a153e72d56208584f99cd5018645fe051d7be7e3
spamulap12 <santhan.pamulapati@nokia.com>
2016-12-08 21:49:04 +0000

    fixed permission deined issue

--- test/scripts/deploy_ci.sh
+++ test/scripts/deploy_ci.sh
@@ -3,7 +3,7 @@ cp ./test/files/setup.yml.CI setup.yml
 ansible-playbook setup.yml -vvvv
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
-#./metro-ansible ci_predeploy.yml -vvvv
+./metro-ansible ci_predeploy.yml -vvvv
 ./metro-ansible ci_deploy.yml -vvvv
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
