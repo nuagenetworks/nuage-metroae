2ab9b1ea991605bf0cb092a5845cfe4120279f6b
spamulap12 <santhan.pamulapati@nokia.com>
2017-01-03 21:00:01 +0000

    adding support for rest of the jenkins jobs

--- test/scripts/deploy_ci.sh
+++ test/scripts/deploy_ci.sh
@@ -7,7 +7,12 @@ ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible ci_predeploy.yml -vvvv
 ./metro-ansible ci_deploy.yml -vvvv
+
+cp ./test/files/setup.yml .
+ansible-playbook setup.yml -vvvv
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
-./metro-ansible test.yml -vvvv
+./metro-ansible test_install.yml -vvvv
+./metro-ansible test_cleanup.yml -vvvv
+
 ./metro-ansible ci_destroy.yml -vvvv
--- test/scripts/deploy_ci_all.sh
+++ test/scripts/deploy_ci_all.sh
@@ -26,10 +26,11 @@ ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible test_install.yml -vvvv
 
-#./metro-ansible ci_destroy.yml -vvvv
-#cp ./test/files/setup.yml ./setup.yml
-#ansible-playbook setup.yml -vvvv
-#ansible-playbook reset_build.yml -vvvv
-#ansible-playbook build.yml -vvvv
-#./metro-ansible test_cleanup.yml -vvvv
+cp ./test/files/setup.yml ./setup.yml
+ansible-playbook setup.yml -vvvv
+ansible-playbook reset_build.yml -vvvv
+ansible-playbook build.yml -vvvv
+./metro-ansible test_cleanup.yml -vvvv
+
+./metro-ansible ci_destroy.yml -vvvv
 
