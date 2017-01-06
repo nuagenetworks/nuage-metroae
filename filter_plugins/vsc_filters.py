f7e75b37cf33e7dd7bd02234c96e86ad439cb3b2
root <root@ansible.caso>
2017-01-06 11:06:28 -0800

    enhanced custom filters using regex

--- filter_plugins/vsc_filters.py
+++ filter_plugins/vsc_filters.py
@@ -18,6 +18,20 @@ import re
 #    under the License.
 
 
+def name_value_helper(name, delimiter, string):
+    ''' Matches "name <delimiter> <value>" in the string and returns a JSON
+    snippet of the form '"name": "value",'. Whitespace around the delimiter
+    is optional.
+    '''
+    VALUE_RE = "\s*" + delimiter + "\s*(\S+)\s+"
+    scratch = re.search(name+VALUE_RE, string)
+    json = "\"" + name + "\": "
+    if scratch:
+        json += "\"" + scratch.group(1) + "\""
+    else:
+        json += "\"0\""
+    return json
+
 def bgp_summary_to_json(string):
     ''' Given a string representation of the output of "show router bgp summary"
     as a string, return a JSON representation of a subset of the data in that output.
@@ -30,76 +44,34 @@ def bgp_summary_to_json(string):
         {
           "IP addr": "1.1.1.2",
           "Uptime": "01h02m03s",
-          "IPV4_counts": "0/0/0",
-          "evpn_counts": "0/0/31"
+          "IPV4 counts": "0/0/0",
+          "evpn counts": "0/0/31"
         },
         {
           "IP addr": "1.1.1.3",
           "Uptime": "01h02m05s",
-          "IPV4_counts": "0/0/0",
-          "evpn_counts": "7/0/24"
+          "IPV4 counts": "0/0/0",
+          "evpn counts": "7/0/24"
         }
       ]
     }
     '''
-    IP_ADDR_COMP_REGEX = "(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])"
-    IP_ADDR_REGEX = "(?:(?:" + IP_ADDR_COMP_REGEX + "\.){3}" + IP_ADDR_COMP_REGEX + ")"
-    UPTIME_REGEX = "(?:[0-2][0-9]h[0-5][0-9]m[0-5][0-9]s)"
-    CONN_REGEX = "(?:" + IP_ADDR_REGEX + "(?:\s+[0-9]+\s+[0-9]+\s+[0-9]+\s+)" + UPTIME_REGEX + ")"
     BGP_ADMIN_STATE = "BGP Admin State"
     BGP_OPER_STATE = "BGP Oper State"
     TOTAL_PEERS = "Total Peers"
+    PEER_RE = "\s+((?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s+\S+\s+\S+\s+\S+\s+(\S+)\s+(\S+)\s+\S+\s+\S+\s+\S+\s+(\S+)\s+\S+"
     json = "{"
-    scratch = string.split('\n')
-    found = False
-    for line in scratch:
-        if BGP_ADMIN_STATE in line:
-            json += "\"" + BGP_ADMIN_STATE + "\": "
-            if len(line.split(':')) < 2:
-                raise AnsibleError(BGP_ADMIN_STATE + ' output unexpected format')
-            json += "\"" + line.split(':')[1].split()[0].strip() + "\",\n"
-            found = True
-            break
-    if not found:
-        raise AnsibleError(BGP_ADMIN_STATE + ' not found')
-    found = False
-    for line in scratch:
-        if BGP_OPER_STATE in line:
-            json += "\"" + BGP_OPER_STATE + "\": "
-            if len(line.split(':')) < 3:
-                raise AnsibleError(BGP_OPER_STATE + ' output unexpected format')
-            json += "\"" + line.split(':')[2].strip() + "\","
-            found = True
-            break
-    if not found:
-        raise AnsibleError(BGP_OPER_STATE + ' not found')
-    found = False
-    for line in scratch:
-        if TOTAL_PEERS in line:
-            json += "\"" + TOTAL_PEERS + "\": "
-            if len(line.split(':')) < 3:
-                raise AnsibleError(TOTAL_PEERS + ' output unexpected format')
-            json += "\"" + line.split(':')[2].strip() + "\","
-            found = True
-            break
-    if not found:
-        raise AnsibleError(TOTAL_PEERS + ' not found')
+    json += name_value_helper(BGP_ADMIN_STATE, ':', string) + ","
+    json += name_value_helper(BGP_OPER_STATE, ':', string) + ","
+    json += name_value_helper(TOTAL_PEERS, ':', string) + ","
     json += "\"Peers\": ["
-    if len(string.split('Neighbor')) < 2:
-        raise AnsibleError('Neighbor information missing')
-    connections = string.split('Neighbor')[1].strip()
-    if len(re.split('-+', connections)) < 2:
-        raise AnsibleError('Neighbor information unexpected format')
-    connections = re.split('-+', connections)[1]
-    connections = connections.replace('\n', '\t').strip()
-    matches = re.findall(CONN_REGEX, connections)
-    for match in matches:
-        connection = match.split('\t')
-        if len(connection) < 2 or len(connection[1].split()) < 4:
-            raise AnsibleError('Peer information unexpected format')
-        json += "{\"IP Addr\": \"" + connection[0].strip() + "\","
-        json += "\"Uptime\": \"" + connection[1].split()[3].strip() + "\"}"
-        if line != matches[-1]:
+    peers = re.findall(PEER_RE, string)
+    for peer in peers:
+        json += "{\"IP Addr\": \"%s\"," % (peer[0])
+        json += "\"Uptime\": \"%s\"," % (peer[1])
+        json += "\"IPV4 counts\": \"%s\"," % (peer[2])
+        json += "\"evpn counts\": \"%s\"}" % (peer[3])
+        if peer != peers[-1]:
             json += ","
     json += "]}"
     return json
@@ -119,40 +91,9 @@ def xmpp_server_detail_to_json(string):
     XMPP_USER_NAME = "XMPP User Name"
     STATE = "State"
     json = "{"
-    scratch = string.split('\n')
-    found = False
-    for line in scratch:
-        if XMPP_FQDN in line:
-            json += "\"" + XMPP_FQDN + "\": "
-            if len(line.split()) < 4:
-                raise AnsibleError(XMPP_FQDN + ' output unexpected format')
-            json += "\"" + line.split()[3].strip() + "\","
-            found = True
-            break
-    if not found:
-        raise AnsibleError(XMPP_FQDN + ' not found')
-    found = False
-    for line in scratch:
-        if XMPP_USER_NAME in line:
-            json += "\"" + XMPP_USER_NAME + "\": "
-            if len(line.split()) < 5:
-                raise AnsibleError(XMPP_USER_NAME + ' output unexpected format')
-            json += "\"" + line.split()[4].strip() + "\","
-            found = True
-            break
-    if not found:
-        raise AnsibleError(XMPP_USER_NAME + ' not found')
-    found = False
-    for line in scratch:
-        if STATE in line:
-            json += "\"" + STATE + "\": "
-            if len(line.split()) < 3:
-                raise AnsibleError(STATE + ' output unexpected format')
-            json += "\"" + line.split()[2].strip() + "\""
-            found = True
-            break
-    if not found:
-        raise AnsibleError(STATE + ' not found')
+    json += name_value_helper(XMPP_FQDN, ':', string) + ","
+    json += name_value_helper(XMPP_USER_NAME, ':', string) + ","
+    json += name_value_helper(STATE, ':', string)
     json += "}"
     return json
 
@@ -166,18 +107,7 @@ def show_vswitches_to_json(string):
     }
     '''
     NUMVSWITCHES = "No. of virtual switches"
-    json = "{\"" + NUMVSWITCHES + "\": "
-    scratch = string.split('\n')
-    found = False
-    for line in scratch:
-        if NUMVSWITCHES in line:
-            if len(line.split(':')) < 2:
-                raise AnsibleError(NUMVSWITCHES + ' output unexpected format')
-            json += "\"" + line.split(':')[1].strip() + "\","
-            found = True
-            break
-    if not found:
-        json += "\"0\","
+    json = "{" + name_value_helper(NUMVSWITCHES, ':', string)
     json += "}"
     return json
 
@@ -192,18 +122,7 @@ def show_vports_to_json(string):
     }
     '''
     NUMVPORTS = "No. of virtual ports"
-    json = "{\"" + NUMVPORTS + "\": "
-    scratch = string.split('\n')
-    found = False
-    for line in scratch:
-        if NUMVPORTS in line:
-            if len(line.split(':')) < 2:
-                raise AnsibleError(NUMVPORTS + ' output unexpected format')
-            json += "\"" + line.split(':')[1].strip() + "\","
-            found = True
-            break
-    if not found:
-        json += "\"0\","
+    json = "{" + name_value_helper(NUMVPORTS, ':', string)
     json += "}"
     return json
 
