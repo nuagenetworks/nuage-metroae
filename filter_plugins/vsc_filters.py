1bcbcda674e0945127ae2715916c6baf3cd70419
root <root@ansible.caso>
2017-01-12 14:38:28 -0800

    converted to use python json library to generate json

--- filter_plugins/vsc_filters.py
+++ filter_plugins/vsc_filters.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import re
+import json
 
 # Copyright 2017 Nokia
 #
@@ -17,19 +18,28 @@ import re
 #    under the License.
 
 
-def name_value_helper(name, delimiter, string):
-    ''' Matches "name <delimiter> <value>" in the string and returns a JSON
-    snippet of the form '"name": "value",'. Whitespace around the delimiter
-    is optional.
+def numeric_name_value_helper(name, delimiter, string):
+    ''' Matches "name <delimiter> <numeric_value>" in the string and returns <numeric_value>
+    If no match, returns 0.
+    '''
+    VALUE_RE = "\s*" + delimiter + "\s*(\d+)\s+"
+    scratch = re.search(name + VALUE_RE, string)
+    if scratch:
+        return scratch.group(1)
+    else:
+        return "0"
+
+
+def string_name_value_helper(name, delimiter, string):
+    ''' Matches "name <delimiter> <text_value>" in the string and returns <text_value>
+    If no match, returns "None"
     '''
     VALUE_RE = "\s*" + delimiter + "\s*(\S+)\s+"
     scratch = re.search(name + VALUE_RE, string)
-    json = "\"" + name + "\": "
     if scratch:
-        json += "\"" + scratch.group(1) + "\""
+        return scratch.group(1)
     else:
-        json += "\"0\""
-    return json
+        return "None"
 
 
 def bgp_summary_to_json(string):
@@ -61,21 +71,22 @@ def bgp_summary_to_json(string):
     BGP_OPER_STATE = "BGP Oper State"
     TOTAL_PEERS = "Total Peers"
     PEER_RE = "\s+((?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s+\S+\s+\S+\s+\S+\s+(\S+)\s+(\S+)\s+\S+\s+\S+\s+\S+\s+(\S+)\s+\S+"
-    json = "{\"Command\": \"show router bgp summary\","
-    json += name_value_helper(BGP_ADMIN_STATE, ':', string) + ","
-    json += name_value_helper(BGP_OPER_STATE, ':', string) + ","
-    json += name_value_helper(TOTAL_PEERS, ':', string) + ","
-    json += "\"Peers\": ["
+    dict = {}
+    peer_list = []
+    dict["Command"] = "show router bgp summary"
+    dict[BGP_ADMIN_STATE] = string_name_value_helper(BGP_ADMIN_STATE, ':', string) + ","
+    dict[BGP_OPER_STATE] = string_name_value_helper(BGP_OPER_STATE, ':', string) + ","
+    dict[TOTAL_PEERS] = numeric_name_value_helper(TOTAL_PEERS, ':', string) + ","
     peers = re.findall(PEER_RE, string)
     for peer in peers:
-        json += "{\"IP Addr\": \"%s\",\n" % (peer[0])
-        json += "\"Uptime\": \"%s\",\n" % (peer[1])
-        json += "\"IPV4 counts\": \"%s\",\n" % (peer[2])
-        json += "\"evpn counts\": \"%s\"}" % (peer[3])
-        if peer != peers[-1]:
-            json += ","
-    json += "]}"
-    return json
+        peer_dict = {}
+        peer_dict["IP Addr"] = peer[0]
+        peer_dict["Uptime"] = peer[1]
+        peer_dict["IPV4 counts"] = peer[2]
+        peer_dict["evpn counts"] = peer[3]
+        peer_list.append(peer_dict)
+    dict["Peers"] = peer_list
+    return json.dumps(dict)
 
 
 def xmpp_server_detail_to_json(string):
@@ -92,12 +103,12 @@ def xmpp_server_detail_to_json(string):
     XMPP_FQDN = "XMPP FQDN"
     XMPP_USER_NAME = "XMPP User Name"
     STATE = "State"
-    json = "{\"Command\": \"show vswitch-controller xmpp-server detail\","
-    json += name_value_helper(XMPP_FQDN, ':', string) + ","
-    json += name_value_helper(XMPP_USER_NAME, ':', string) + ","
-    json += name_value_helper(STATE, ':', string)
-    json += "}"
-    return json
+    dict = {}
+    dict["Command"] = "show vswitch-controller xmpp-server detail"
+    dict[XMPP_FQDN] = string_name_value_helper(XMPP_FQDN, ':', string)
+    dict[XMPP_USER_NAME] = string_name_value_helper(XMPP_USER_NAME, ':', string)
+    dict[STATE] = string_name_value_helper(STATE, ':', string)
+    return json.dumps(dict)
 
 
 def show_vswitches_to_json(string):
@@ -125,19 +136,20 @@ def show_vswitches_to_json(string):
     '''
     NUMVSWITCHES = "No. of virtual switches"
     INSTANCE_RE = "(\S+)\s+(\S+)\s+(\S+d \S+:\S+:\S+)\s+(\S+)"
-    json = "{\"Command\": \"show vswitch-controller vswitches\","
-    json += name_value_helper(NUMVSWITCHES, ':', string) + ","
-    json += "\"Instances\": ["
+    dict = {}
+    inst_list = []
+    dict["Command"] = "show vswitch-controller vswitches"
+    dict[NUMVSWITCHES] = numeric_name_value_helper(NUMVSWITCHES, ':', string)
     instances = re.findall(INSTANCE_RE, string)
     for instance in instances:
-        json += "{\"vswitch-instance\": \"%s\"," % (instance[0])
-        json += "\"Personality\": \"%s\"," % (instance[1])
-        json += "\"Uptime\": \"%s\"," % (instance[2])
-        json += "\"Num\": \"%s\"}" % (instance[3])
-        if instance != instances[-1]:
-            json += ","
-    json += "]}"
-    return json
+        inst_dict = {}
+        inst_dict["vswitch-instance"] = instance[0]
+        inst_dict["Personality"] = instance[1]
+        inst_dict["Uptime"] = instance[2]
+        inst_dict["Num"] = instance[3]
+        inst_list.append(inst_dict)
+    dict["Instances"] = inst_list
+    return json.dumps(dict)
 
 
 def show_host_vports_to_json(string):
@@ -146,13 +158,45 @@ def show_host_vports_to_json(string):
     A sample of the output:
     {
       "Command": "show vswitch-controller vports type host detail",
-      "No. of virtual ports": "12"
+      "No. of virtual ports": "12",
+      "Vports": [
+        {
+          "VP Name": "va-10.15.2.254/1/1",
+          "G/W PortName": "eth2",
+          "VLAN ID": "11",
+          "VPRN": "20159",
+          "EVPN": "20162",
+          "VP IP Address": "70.70.70.11/24"
+        },
+        {
+          "VP Name": "va-10.15.33.254/1/1",
+          "G/W PortName": "eth2",
+          "VLAN ID": "11",
+          "VPRN": "20159",
+          "EVPN": "20162",
+          "VP IP Address": "70.70.70.11/24"
+        }
+      ]
     }
     '''
     NUMVPORTS = "No. of virtual ports"
-    json = "{\"Command\": \"show vswitch-controller vports type host detail\"," + name_value_helper(NUMVPORTS, ':', string)
-    json += "}"
-    return json
+    VPORTS_RE = "(\S+)\s+(\S+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+\.\d+\.\d+\.\d+\/\d+)"
+    dict = {}
+    port_list = []
+    dict["Command"] = "show vswitch-controller vports type host detail"
+    dict[NUMVPORTS] = numeric_name_value_helper(NUMVPORTS, ':', string)
+    ports = re.findall(VPORTS_RE, string)
+    for port in ports:
+        port_dict = {}
+        port_dict["VP Name"] = port[0]
+        port_dict["G/W PortName"] = port[1]
+        port_dict["VLAN ID"] = port[2]
+        port_dict["VPRN"] = port[3]
+        port_dict["EVPN"] = port[4]
+        port_dict["VP IP Address"] = port[5]
+        port_list.append(port_dict)
+    dict["Vports"] = port_list
+    return json.dumps(dict)
 
 
 def show_vm_vports_to_json(string):
@@ -161,13 +205,48 @@ def show_vm_vports_to_json(string):
     A sample of the output:
     {
       "Command": "show vswitch-controller vports type vm detail",
-      "No. of virtual ports": "12"
+      "No. of virtual ports": "12",
+      "Vports": [
+        {
+          "VP Name": "va-10.15.2.254/1/1",
+          "VM Name": "ovs-1-vm1",
+          "VPRN": "20159",
+          "EVPN": "20162",
+          "Multicast": "Disabled",
+          "VP IP Address": "70.70.70.11/24",
+          "Mac Address": "00:14:51:59:09:13"
+        },
+        {
+          "VP Name": "va-10.15.33.254/1/2",
+          "VM Name": "ovs-1-vm2",
+          "VPRN": "20159",
+          "EVPN": "20163",
+          "Multicast": "Disabled",
+          "VP IP Address": "70.70.70.104/24",
+          "Mac Address": "00:14:51:59:09:15"
+        }
+      ]
     }
     '''
     NUMVPORTS = "No. of virtual ports"
-    json = "{\"Command\": \"show vswitch-controller vports type vm detail\"," + name_value_helper(NUMVPORTS, ':', string)
-    json += "}"
-    return json
+    VPORTS_RE = "(\S+)\s+(\S+)\s+(\d+)\s+(\d+)\s+(\S+)\s+(\d+\.\d+\.\d+\.\d+\/\d+)\s+(\d+:\d+:\d+:\d+:\d+:\d+)"
+    dict = {}
+    port_list = []
+    dict["Command"] = "show vswitch-controller vports type vm detail"
+    dict[NUMVPORTS] = numeric_name_value_helper(NUMVPORTS, ':', string)
+    ports = re.findall(VPORTS_RE, string)
+    for port in ports:
+        port_dict = {}
+        port_dict["VP Name"] = port[0]
+        port_dict["VM Name"] = port[1]
+        port_dict["VPRN"] = port[2]
+        port_dict["EVPN"] = port[3]
+        port_dict["Multicast"] = port[4]
+        port_dict["VP IP Address"] = port[5]
+        port_dict["Mac Address"] = port[6]
+        port_list.append(port_dict)
+    dict["Vports"] = port_list
+    return json.dumps(dict)
 
 
 class FilterModule(object):
