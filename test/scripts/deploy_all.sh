c561e84655b17a476e242d93d5f64f398319c83b
shreya2602 <shreya.pagedar@gmail.com>
2017-02-06 12:02:22 -0500

    Files and changes to support VNS only deployment

--- test/scripts/deploy_all.sh
+++ test/scripts/deploy_all.sh
@@ -26,6 +26,12 @@ ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible test_install.yml -vvvv
 
+cp ./test/files/setup.yml.VNSOnly ./setup.yml
+ansible-playbook setup.yml -vvvv
+ansible-playbook reset_build.yml -vvvv
+ansible-playbook build.yml -vvvv
+./metro-ansible test_install.yml -vvvv
+
 cp ./test/files/setup.yml ./setup.yml
 ansible-playbook setup.yml -vvvv
 ansible-playbook reset_build.yml -vvvv
