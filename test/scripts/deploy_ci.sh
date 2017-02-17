384416d9b4ce371eeb5558be31eeb6fcc9b822f2
wasanthag <wasanthag1302@gmail.com>
2017-02-17 11:45:25 -0500

    more tests

--- test/scripts/deploy_ci.sh
+++ test/scripts/deploy_ci.sh
@@ -6,7 +6,7 @@ ansible-playbook setup.yml -vvvv
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible ci_predeploy.yml -vvvv
-./metro-ansible ci_deploy.yml -vvvv
+#./metro-ansible ci_deploy.yml -vvvv
 
 cp ./test/files/setup.yml .
 ansible-playbook setup.yml -vvvv
