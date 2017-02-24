acf9968bd257967748140a17aff12ce4eb551006
spamulap12 <santhan.pamulapati@nokia.com>
2017-02-23 21:01:04 -0800

    adding vnsonly files for ci on openstack

--- test/scripts/deploy_ci_all.sh
+++ test/scripts/deploy_ci_all.sh
@@ -36,7 +36,13 @@ cp ./test/files/setup.yml.VNSOnly ./setup.yml
 ansible-playbook setup.yml -vvvv
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
-./metro-ansible test_install.yml -vvvv
+./metro-ansible test_cleanup.yml -vvvv
+
+cp ./test/files/setup.yml.VNSOnlyWithVSC ./setup.yml
+ansible-playbook setup.yml -vvvv
+ansible-playbook reset_build.yml -vvvv
+ansible-playbook build.yml -vvvv
+./metro-ansible install_vns.yml -vvvv
 
 cp ./test/files/setup.yml ./setup.yml
 ansible-playbook setup.yml -vvvv
