776e02bf4fc6942e17e663f8e1ef6e2e10ad21ec
brcastel <caso@caso-brian-u>
2017-03-07 09:04:52 -0800

    fix unpack build error

--- test/scripts/unpack_4.0R4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-cp ./test/files/setup_4.0R4.yml setup.yml
-ansible-playbook setup.yml -vvvv
-ansible-playbook reset_build.yml -vvvv
-ansible-playbook build.yml -vvvv
-./metro-ansible test_unpack -vvvv
