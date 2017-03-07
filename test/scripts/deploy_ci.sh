8dd249892f21aee6b574996a3345494411ddd23e
root <root@jen-master.localdomain>
2017-03-07 14:23:32 -0800

    changing CI test files

--- test/scripts/deploy_ci.sh
+++ test/scripts/deploy_ci.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -e
+USAGE="Usage: $0 version"
+
+if [ $# -ne 1 ];
+then
+    echo "Requires exactly 1 argument"
+    echo $USAGE
+    exit 1
+fi
 
 cp ./test/files/setup.yml.CI setup.yml
 ansible-playbook setup.yml -vvvv
@@ -8,8 +16,11 @@ ansible-playbook build.yml -vvvv
 ./metro-ansible ci_predeploy.yml -vvvv
 ./metro-ansible ci_deploy.yml -vvvv
 
-cp ./test/files/setup.yml .
-ansible-playbook setup.yml -vvvv
+cp ./test/files/build_vars.yml.all roles/reset-build/files/build_vars.yml
+sed -i "s/VERSION/$1/g" roles/reset-build/files/build_vars.yml
+cp ./test/files/test_install.yml .
+cp ./test/files/test_cleanup.yml .
+
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible test_install.yml -vvvv
