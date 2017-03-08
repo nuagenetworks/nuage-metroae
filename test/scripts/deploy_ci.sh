a454c570f98b8641843788432d4fcccd1782977e
wasanthag <wasanthag1302@gmail.com>
2017-03-08 15:53:34 -0500

    update ci configs to match setup file removal

--- test/scripts/deploy_ci.sh
+++ test/scripts/deploy_ci.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+USAGE="Usage: $0 version"
+
+if [ $# -ne 1 ];
+then
+    echo "Requires exactly 1 argument"
+    echo $USAGE
+    exit 1
+fi
 
 cp ./test/files/setup.yml.CI setup.yml
 ansible-playbook setup.yml -vvvv
@@ -8,8 +17,19 @@ ansible-playbook build.yml -vvvv
 ./metro-ansible ci_predeploy.yml -vvvv
 ./metro-ansible ci_deploy.yml -vvvv
 
-cp ./test/files/setup.yml .
-ansible-playbook setup.yml -vvvv
+
+if [ $1 = 4.0R4 ];
+then
+    cp ./test/files/build_vars_4.0R4.yml.all roles/reset-build/files/build_vars.yml
+
+else
+    cp ./test/files/build_vars.yml.all roles/reset-build/files/build_vars.yml
+fi
+
+cp ./test/files/test_install.yml .
+cp ./test/files/test_cleanup.yml .
+sed -i "s/VERSION/$1/g" roles/reset-build/files/build_vars.yml
+
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible test_install.yml -vvvv
