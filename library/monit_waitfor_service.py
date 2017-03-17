6d4ee98726738eba7724b38a75614415098856bf
wasanthag <wasanthag1302@gmail.com>
2017-03-17 10:38:38 -0400

    update variable names and module name for monit polling module

--- library/monit_waitfor_service.py
+++ library/monit_waitfor_service.py
@@ -5,45 +5,45 @@ import time
 
 DOCUMENTATION = '''
 ---
-module: service_wait_for
-short_description: Wait for a service/program/process managed by monit to come to active state within a given time period
+module: monit_waitfor_service
+short_description: Wait for a service/program/process managed by monit to come to active state within a given time timeout_seconds
 options:
   name:
     description:
       - The name of the monit program/process
     required: true
     default: null
-  period:
+  timeout_seconds:
     description:
-      - time period to monitor
+      - time timeout_seconds to monitor
     required: true
     default: null
-  frequency:
+  test_interval_seconds:
     description:
-      - frequency of polling
+      - test_interval_seconds of polling
     required: true
     default: null
 '''
 
 EXAMPLES = '''
 # Wait for the state of program "ejabberd-status" to become ok polling every 30 seconds for 600 seconds
-- service_wait_for: name=ejabberd-status period=600 frequency=30
+- monit_waitfor_service: name=ejabberd-status timeout_seconds=600 test_interval_seconds=30
 '''
 
 
 def main():
     arg_spec = dict(
         name=dict(required=True, type='list'),
-        period=dict(required=True, type='int'),
-        frequency=dict(required=True, type='int')
+        timeout_seconds=dict(required=True, type='int'),
+        test_interval_seconds=dict(required=True, type='int')
     )
 
     monit_stats = dict()
     module = AnsibleModule(argument_spec=arg_spec, supports_check_mode=True)
 
     vsd_stats_proc = module.params['name']
-    period = module.params['period']
-    frequency = module.params['frequency']
+    timeout_seconds = module.params['timeout_seconds']
+    test_interval_seconds = module.params['test_interval_seconds']
 
     MONIT = module.get_bin_path('monit', True)
 
@@ -69,12 +69,12 @@ def main():
 
     for proc_name in vsd_stats_proc:
         proc_status = status(proc_name)
-        while desired_state == False and time_elapsed < period:
+        while desired_state == False and time_elapsed < timeout_seconds:
               if proc_status == 'ok' or proc_status == 'running':
                  desired_state = True
               else:
-                 time.sleep(frequency)
-                 time_elapsed = time_elapsed + frequency
+                 time.sleep(test_interval_seconds)
+                 time_elapsed = time_elapsed + test_interval_seconds
                  proc_status = status(proc_name)
 
         monit_stats[proc_name] = proc_status
@@ -84,7 +84,7 @@ def main():
     if desired_state == True:
         module.exit_json(changed=True, name=proc_name, state=monit_stats)
     else:
-        module.fail_json(msg="Process %s did not transitioned to active within %i seconds" % (proc_name, period))
+        module.fail_json(msg="Process %s did not transitioned to active within %i seconds" % (proc_name, timeout_seconds))
         # Run the main
 
 
