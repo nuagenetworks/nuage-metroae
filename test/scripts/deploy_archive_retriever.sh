9f893fdfc47b6a3f16007582d5ece77a09b2f8f3
Brian Castelli <bacastelli@pobox.com>
2017-03-21 16:34:28 -0400

    Remove unnecessary restore of hosts file

--- test/scripts/deploy_archive_retriever.sh
+++ test/scripts/deploy_archive_retriever.sh
@@ -4,5 +4,3 @@ set -e
 sed -i "s/TARGET_HOST/$1/g" test/archive_retriever/hosts
 
 ./archive-ansible test/archive_retriever/archive_retriever.yml -vvvv
-
-sed -i "s/$1/TARGET_HOST/g" test/archive_retriever/hosts
