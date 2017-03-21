234a7aa6bcc6ccd7684f0ff45ed778af85bc2ba1
brcastel <caso@caso-brian-u>
2017-03-21 13:15:16 -0700

    fix up OS-based Jenkins builds

--- test/scripts/deploy_ci.sh
+++ test/scripts/deploy_ci.sh
@@ -27,15 +27,10 @@ cp ./test/files/setup.yml.CI setup.yml
 ansible-playbook setup.yml -vvvv
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
-if [ $1 = 4.0R4 ] || [ $1 = 3.2R10 ];
-then
-    sed -i  '/- { hostname: {{ vrs_u16_target_server_name }},/,/ci_flavor: jenkins }/ d' test/files/build_vars.yml.all.j2
-    sed -i '/- { vrs_os_type: u16.04,/,/standby_controller_ip: {{ network_address }}.213 }/d' test/files/build_vars.yml.all.j2
-fi
 ./metro-ansible ci_predeploy.yml -vvvv
 ./metro-ansible ci_deploy.yml -vvvv
 
-cp ./test/files/build_vars.yml.all roles/reset-build/files/build_vars.yml
+cp ./test/files/build_vars_all.yml roles/reset-build/files/build_vars.yml
 cp ./test/files/test_install.yml .
 cp ./test/files/test_cleanup.yml .
 sed -i "s/VERSION/$1/g" roles/reset-build/files/build_vars.yml
--- test/scripts/deploy_ci_all.sh
+++ test/scripts/deploy_ci_all.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 set -e
+USAGE="Usage: $0 version"
+
+if [ $# -ne 1 ];
+then
+    echo "Requires exactly 1 argument"
+    echo $USAGE
+    exit 1
+fi
+
+USAGE="Usage: $0 version"
+
+if [ $# -ne 1 ];
+then
+    echo "Requires exactly 1 argument"
+    echo $USAGE
+    exit 1
+fi
+
+if [ $1 = 4.0R4 ] || [ $1 = 3.2R10 ];
+then
+    sed -i  '/- { hostname: {{ vrs_u16_host_name }},/,/ci_flavor: m1.medium }/d' test/files/build_vars.yml.CI.j2
+fi
+
 cp ./test/files/setup.yml.CI setup.yml
 ansible-playbook setup.yml -vvvv
 ansible-playbook reset_build.yml -vvvv
@@ -7,44 +30,47 @@ ansible-playbook build.yml -vvvv
 ./metro-ansible ci_predeploy.yml -vvvv
 ./metro-ansible ci_deploy.yml -vvvv
 
-cp ./test/files/setup.yml.VSDOnly ./setup.yml
-ansible-playbook setup.yml -vvvv
+cp ./test/files/test_install.yml .
+cp ./test/files/test_cleanup.yml .
+
+cp ./test/files/build_vars_vsdonly.yml roles/reset-build/files/build_vars.yml
+sed -i "s/VERSION/$1/g" roles/reset-build/files/build_vars.yml
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible test_install.yml -vvvv
 
-cp ./test/files/setup.yml.VSCOnly ./setup.yml
-ansible-playbook setup.yml -vvvv
+cp ./test/files/build_vars_vsconly.yml roles/reset-build/files/build_vars.yml
+sed -i "s/VERSION/$1/g" roles/reset-build/files/build_vars.yml
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible test_install.yml -vvvv
 
-cp ./test/files/setup.yml.VSTATOnly ./setup.yml
-ansible-playbook setup.yml -vvvv
+cp ./test/files/build_vars_vstatonly.yml roles/reset-build/files/build_vars.yml
+sed -i "s/VERSION/$1/g" roles/reset-build/files/build_vars.yml
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible test_install.yml -vvvv
 
-cp ./test/files/setup.yml.VRSOnly ./setup.yml
-ansible-playbook setup.yml -vvvv
+cp ./test/files/build_vars_vrsonly.yml roles/reset-build/files/build_vars.yml
+sed -i "s/VERSION/$1/g" roles/reset-build/files/build_vars.yml
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible test_install.yml -vvvv
 
-cp ./test/files/setup.yml.VNSOnly ./setup.yml
-ansible-playbook setup.yml -vvvv
+cp ./test/files/build_vars_vnsonly.yml roles/reset-build/files/build_vars.yml
+sed -i "s/VERSION/$1/g" roles/reset-build/files/build_vars.yml
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible test_cleanup.yml -vvvv
 
-cp ./test/files/setup.yml.VNSOnlyWithVSC ./setup.yml
-ansible-playbook setup.yml -vvvv
+cp ./test/files/build_vars_vnsonlywithvsc.yml roles/reset-build/files/build_vars.yml
+sed -i "s/VERSION/$1/g" roles/reset-build/files/build_vars.yml
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible install_vns.yml -vvvv
 
-cp ./test/files/setup.yml ./setup.yml
-ansible-playbook setup.yml -vvvv
+cp ./test/files/build_vars_all.yml roles/reset-build/files/build_vars.yml
+sed -i "s/VERSION/$1/g" roles/reset-build/files/build_vars.yml
 ansible-playbook reset_build.yml -vvvv
 ansible-playbook build.yml -vvvv
 ./metro-ansible test_cleanup.yml -vvvv
