9cd618ad2ab4a1bbc396e961fbff5e305fa0d19d
spamulap12 <santhan.pamulapati@nokia.com>
2017-05-17 16:01:39 -0700

    fixing typo

--- test/scripts/upgrade_sa_vstat.sh
+++ test/scripts/upgrade_sa_vstat.sh
@@ -11,7 +11,7 @@ then
 fi
 
 cp ./test/files/build_vars.yml.standalone_vsd_vstat roles/reset-build/files/build_vars.yml
-cp ./test/files/upgrade_vars.yml.standalone_vsd_vstat roles/reset-build/files/upgrade_vars.yml
+cp ./test/files/upgrade_vars.yml.standalone_vsd roles/reset-build/files/upgrade_vars.yml
 cp ./test/files/test_install.yml .
 cp ./test/files/test_cleanup.yml .
 cp ./test/files/user_creds.yml.standalone_vsd ./user_creds.yml
