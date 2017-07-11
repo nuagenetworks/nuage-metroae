c18c6a3fdf3aee8afb559d35f1b0921d2782f6c9
wasanthag <wasanthag1302@gmail.com>
2017-07-11 14:21:58 +0000

    update rpm build

--- rpms/rpm_build.sh
+++ rpms/rpm_build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-cp rpms/metro.spec SPECS
+echo "Create RPM Build Env"
+rpmdev-setuptree 
+cp ./rpms/metro.spec ~/rpmbuild/SPECS/
+mkdir -p ~/rpmbuild/SOURCES/metro-2.1.2/opt/nuage-metro-2.1.2/
+cp -r . ~/rpmbuild/SOURCES/metro-2.1.2/opt/nuage-metro-2.1.2/
+tar -zcvf ~/rpmbuild/SOURCES/metro-2.1.2.tar.gz ~/rpmbuild/SOURCES/metro-2.1.2
+echo "Completed creating RPM build Env"
+
+echo "Building metro RPM"
+rpmbuild -v -bb ~/rpmbuild/SPECS/metro.spec
+echo "Completed creating RPM"
+
+cp -r ~/rpmbuild/RPMS/*.rpm /home/caso/nfs-data/misc/
+
+
