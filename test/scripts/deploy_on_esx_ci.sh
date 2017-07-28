fa64769723e6c6d218520f585bb0228a928eb769
Madhukar Krishnarao <madhukark@gmail.com>
2017-07-28 11:16:35 -0700

    reset deploy_on_esx_ci

--- test/scripts/deploy_on_esx_ci.sh
+++ test/scripts/deploy_on_esx_ci.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-USAGE="Usage: $0 version testbedID"
+USAGE="Usage: $0 version"
 
-if [ $# -ne 2 ];
+if [ $# -ne 1 ];
 then
-    echo "Requires exactly 2 arguments"
+    echo "Requires exactly 1 argument"
     echo $USAGE
     exit 1
 fi
@@ -14,14 +14,9 @@ cp ./test/files/build_vars_esx.yml roles/reset-build/files/build_vars.yml
 cp ./test/files/test_install_on_esx.yml .
 cp ./test/files/test_cleanup_on_esx.yml .
 
-TESTBED_ID=$2
-TESTBED_IP=$((TESTBED_ID+1))
-
 sed -i "s/VERSION/$1/g" roles/reset-build/files/build_vars.yml
-sed -i "s/TESTBED_ID/$TESTBED_ID/g" roles/reset-build/files/build_vars.yml
-sed -i "s/TESTBED_IP/$TESTBED_IP/g" roles/reset-build/files/build_vars.yml
 
 ./metro-ansible reset_build.yml -vvvv
 ./metro-ansible build.yml -vvvv
 ./metro-ansible test_install_on_esx.yml -vvvv
-./metro_ansible test_cleanup_on_esx.yml -vvvv
+#./metro_ansible test_cleanup_on_esx.yml -vvvv
