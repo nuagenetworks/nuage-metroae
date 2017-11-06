d32d3fef733c0064a39ebf5ffa0ae8fdfa0173ed
Jeroen van Bemmel <jvb127@gmail.com>
2017-11-06 10:14:10 -0600

    Fix 'execution failed' state of keyserver process on VSD

--- library/monit_waitfor_service.py
+++ library/monit_waitfor_service.py
@@ -76,6 +76,10 @@ def main():
             if proc_status == 'ok' or proc_status == 'running' or proc_status == 'accessible':
                 desired_state = True
             else:
+                # JvB sometimes keyserver-status ends up as 'execution failed' - restart it
+                if proc_status == 'failed':
+                   module.run_command('%s restart %s'
+                                        % (MONIT, proc_name), check_rc=True)
                 time.sleep(test_interval_seconds)
                 time_elapsed = time_elapsed + test_interval_seconds
                 proc_status = status(proc_name)
