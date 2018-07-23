fbada5da5ad70bc26e90f9bc88040794559329da
Aditya Suresh <aditya.suresh@nokia.com>
2018-07-23 10:52:42 -0400

    Changed directory of new python scripts

---
+++ roles/vsd-predeploy/files/get_vcenter_host.py
@@ -0,0 +1,25 @@
+import subprocess
+
+from pyVim import connect
+
+def main():
+    proc = subprocess.Popen(["sudo dmidecode|grep UUID|awk '{print $2}'"], 
+                            stdout=subprocess.PIPE,
+                            shell=True)
+    out, err proc.communicate()
+    uuid = out[:-1]
+
+    SI = None
+    SI = connect.SmartConnect(host=ARGS.host,
+                          user=ARGS.user,
+                          pwd=ARGS.password,
+                          port=ARGS.port)
+    vm = SI.content.searchIndex.FindbyUuid(None,
+                                        uuid,
+                                        True,
+                                        False)
+    host = vm.runtime.host 
+
+    return host
+
+main()
