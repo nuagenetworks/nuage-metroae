d374e93724e04a19937d7714566f3ea65a25b724
Aditya Suresh <aditya.suresh@nokia.com>
2018-07-23 10:25:33 -0400

    Restructured python script to return host info for use in vcenter vsd-predeploy

--- library/get_vcenter_host.py
+++ library/get_vcenter_host.py
@@ -2,21 +2,24 @@ import subprocess
 
 from pyVim import connect
 
-proc = subprocess.Popen(["sudo dmidecode|grep UUID|awk '{print $2}'"], 
-                        stdout=subprocess.PIPE,
-                        shell=True)
-out, err proc.communicate()
-uuid = out[:-1]
+def main():
+    proc = subprocess.Popen(["sudo dmidecode|grep UUID|awk '{print $2}'"], 
+                            stdout=subprocess.PIPE,
+                            shell=True)
+    out, err proc.communicate()
+    uuid = out[:-1]
 
-SI = None
-SI = connect.SmartConnect(host=ARGS.host,
+    SI = None
+    SI = connect.SmartConnect(host=ARGS.host,
                           user=ARGS.user,
                           pwd=ARGS.password,
                           port=ARGS.port)
-vm = SI.content.searchIndex.FindbyUuid(None,
-                                       uuid,
-                                       True,
-                                       False)
-host = vm.runtime.host 
+    vm = SI.content.searchIndex.FindbyUuid(None,
+                                        uuid,
+                                        True,
+                                        False)
+    host = vm.runtime.host 
 
-print "Host name: {}".format(host.name)
+    return host
+
+main()
