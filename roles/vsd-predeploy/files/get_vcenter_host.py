1550baba76d7200a54f72206db6756c87406db6e
Aditya Suresh <aditya.suresh@nokia.com>
2018-07-23 14:51:38 -0400

    Added code to get correct args from VM to get host info

--- roles/vsd-predeploy/files/get_vcenter_host.py
+++ roles/vsd-predeploy/files/get_vcenter_host.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 import subprocess
-
+import argparse
 from pyVim import connect
 
 def main():
+    parser = argparse.ArgumentParser(description='Process args to retrieve VMs')
+    parser.add_argument('-s', '--host', required=True, action='store',
+                        help='Remote host to connect to')
+    parser.add_argument('-o', '--port', type=int, default=443, action='store',
+                        help='Port to connect on')
+    parser.add_argument('-u', '--user', required=True, action='store',
+                        help='User name to use to connect to host')
+    parser.add_argument('-p', '--password', required=False, action='store',
+                        help='Password to use to connect to host')
+    
+    args = parser.parse_args()
     proc = subprocess.Popen(["sudo dmidecode|grep UUID|awk '{print $2}'"], 
                             stdout=subprocess.PIPE,
                             shell=True)
@@ -11,10 +22,10 @@ def main():
     uuid = out[:-1]
 
     SI = None
-    SI = connect.SmartConnect(host=ARGS.host,
-                          user=ARGS.user,
-                          pwd=ARGS.password,
-                          port=ARGS.port)
+    SI = connect.SmartConnect(host=args.host,
+                          user=args.user,
+                          pwd=args.password,
+                          port=args.port)
     vm = SI.content.searchIndex.FindbyUuid(None,
                                         uuid,
                                         True,
