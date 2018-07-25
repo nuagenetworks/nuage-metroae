b752dca2594b061332e3e193c2e48d294f95197e
Aditya Suresh <aditya.suresh@nokia.com>
2018-07-25 10:54:14 -0400

    Checking output

--- roles/vsd-predeploy/files/get_vcenter_host.py
+++ roles/vsd-predeploy/files/get_vcenter_host.py
@@ -7,20 +7,23 @@ from pyVim.connect import SmartConnect
 
 def main():
     parser = argparse.ArgumentParser(description='Process args to retrieve VMs')
-    parser.add_argument('-s', '--host', required=True, action='store',
-                        help='Remote host to connect to')
-    parser.add_argument('-o', '--port', type=int, default=443, action='store',
-                        help='Port to connect on')
-    parser.add_argument('-u', '--user', required=True, action='store',
-                        help='User name to use to connect to host')
-    parser.add_argument('-p', '--password', required=False, action='store',
-                        help='Password to use to connect to host')
+     parser.add_argument('-s', '--host', required=True, action='store',
+                         help='Remote host to connect to')
+     parser.add_argument('-o', '--port', type=int, default=443, action='store',
+                         help='Port to connect on')
+     parser.add_argument('-u', '--user', required=True, action='store',
+                         help='User name to use to connect to host')
+     parser.add_argument('-p', '--password', required=False, action='store',
+                         help='Password to use to connect to host')
     
+    parser.add_argument('-u', '--uuid', required=True, action='store',
+                        help='UUID of VM')
     args = parser.parse_args()
-    proc = subprocess.Popen(["sudo dmidecode|grep UUID|awk '{print $2}'"], 
-                            stdout=subprocess.PIPE,
-                            shell=True)
+     proc = subprocess.Popen(["sudo dmidecode|grep UUID|awk '{print $2}'"], 
+                             stdout=subprocess.PIPE,
+                             shell=True)
     out, err = proc.communicate()
+    print (out)
     uuid = out[:-1]
     print (uuid)
     si = None
