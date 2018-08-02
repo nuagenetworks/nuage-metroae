718fbeabb82ca3cbae5078ca02cb5fc1feda9efa
Aditya Suresh <aditya.suresh@nokia.com>
2018-08-02 16:06:59 -0400

    Checking correct status for show router interface command

--- filter_plugins/vsc_filters.py
+++ filter_plugins/vsc_filters.py
@@ -442,6 +442,23 @@ def vsd_detail_to_json(string):
         dict["VSD-Info"].append(vsd_dict)
     return json.dumps(dict)
 
+def vsc_router_interfaces_to_json(string):
+    dict = {}
+    dict["Command"] = "show router interface"
+    dict["VSC-Interface-Info"] = []
+    vsc_re = re.compile(r'control\s*(?P<control_adm>(\w+))\s*(?P<control_oprv4>(\w+)).(?P<control_oprv6>(\w+))\s*(?P<control_mode>(\w+))\s*(?P<control_port>(\w*)).(?P<control_sapid>(\w.+))\s*(?P<control_ipaddr>(\w*.\w*.\w*.\w*))\s*(?P<control_pfxstate>(\w*.\w*))\s*\w*\s*(?P<system_adm>(\w*))\s*(?P<system_oprv4>(\w*)).(?P<system_oprv6>(\w*))\s*(?P<system_oprv6>(\w*))\s*(?P<system_port>(\w*)).(?P<system.sapid>(\w*.\w*))\s*(?P<system_ip>(\w*.\w*.\w*.\w*)).\w*\s*(?P<system_pfxstate>(\w*.\w*))')
+    vsc_interface_info = re.finditer(vsc_re, string)
+    interface_num = re.compile(r'Interfaces\s*.\s*(?P<num_interfaces>(\w*)')
+    vsc_num_interface = re.finditer(interface_num, string)
+    for vsc in vsc_interface_info:
+        vsc_interface_dict = {"control_Adm" : vsc.group('control_adm'),
+                              "control_Oprv4" : vsc.group('control_oprv4'),
+                              "system_Adm" : vsc.group("system_adm"),
+                              "system_Oprv4" : vsc.group("system_Oprv4")
+                              }
+        dict["VSC-Interface-Info"].append(vsc_interface_dict)
+    return json.dumps(dict)
+
 
 class FilterModule(object):
     ''' Query filter '''
@@ -458,5 +475,6 @@ class FilterModule(object):
             'image_version_to_json': image_version_to_json,
             'show_version_to_json': show_version_to_json,
             'vsc_system_connections_to_json': vsc_system_connections_to_json,
-            'vsd_detail_to_json': vsd_detail_to_json
+            'vsd_detail_to_json': vsd_detail_to_json,
+            'vsc_router_interfaces_to_json': vsc_router_interfaces_to_json
         }
