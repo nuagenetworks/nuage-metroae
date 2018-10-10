d58f4b010648a48f31fba377bc047aa67816870e
Sanket <godbole.sanket@gmail.com>
2018-10-10 16:35:29 -0400

    Add deployment name option

--- encrypt_credentials.py
+++ encrypt_credentials.py
@@ -2,6 +2,7 @@ import sys
 import yaml
 import json
 import getpass
+import argparse
 from ansible.parsing.vault import VaultEditor, VaultSecret, is_encrypted
 
 
@@ -17,11 +18,10 @@ def literal_unicode_representer(dumper, data):
     return dumper.represent_scalar("!vault", data, style='|')
 
 
-def encrypt_credentials_file(passcode):
+def encrypt_credentials_file(passcode, deployment_name):
     yaml.add_representer(VaultYaml, literal_unicode_representer)
     yaml.add_constructor(u'!vault', vault_constructor)
-    # todo: what happens for non-default
-    credentials_file = 'deployments/default/credentials.yml'
+    credentials_file = 'deployments/' + deployment_name + '/credentials.yml'
     with open(credentials_file, 'r') as file:
         credentials = yaml.load(file.read())
 
@@ -48,6 +48,14 @@ def encrypt_credentials_file(passcode):
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description='Encrypt user credentials file')
+    parser.add_argument(
+        "--deployment",
+        help="Optional deployment name - will default to 'default' deployment",
+        default="default")
+    args = parser.parse_args()
+
     try:
         print "This file will ecrypt user credentials for MetroAE"
         print "All user comments in the user credentials file might be lost"
@@ -55,7 +63,7 @@ def main():
     except:
         print "Error in getting passcode from command line"
         sys.exit()
-    encrypt_credentials_file(passcode)
+    encrypt_credentials_file(passcode, args.deployment)
 
 
 if __name__ == '__main__':
