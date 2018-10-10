36a92d13103040e7b3483e02586cc5d411f29523
Sanket <godbole.sanket@gmail.com>
2018-10-10 13:25:23 -0400

    More changes to encrypt credentials

--- encrypt_credentials.py
+++ encrypt_credentials.py
@@ -1,13 +1,17 @@
 import argparse
 import sys
 import yaml
-from ansible.parsing.vault import VaultAES256
-from ansible.parsing.vault import VaultSecret
+import json
+from ansible.parsing.vault import VaultEditor, VaultSecret
 
-def to_bytes(n, length, endianess='big'):
-    h = '%x' % n
-    s = ('0'*(len(h) % 2) + h).zfill(length*2).decode('hex')
-    return s if endianess == 'big' else s[::-1]
+class VaultYaml(unicode): 
+    pass
+
+
+def literal_unicode_representer(dumper, data):
+    return dumper.represent_scalar("!vault", data, style='|')
+
+yaml.add_representer(VaultYaml, literal_unicode_representer)
 
 def encrypt_credentials_file(passcode):
     #todo: what happens for non-default
@@ -16,28 +20,27 @@ def encrypt_credentials_file(passcode):
         credentials = yaml.safe_load(file.read())
 
     print credentials
+
+    with open('schemas/credentials.json') as credentials_schema:    
+        data = yaml.safe_load(json.dumps(json.load(credentials_schema)))
+    props = data['properties']
+    encryt_credentials_list = []
+    for k,v in props.items():
+        if ('encrypt' not in v) or v['encrypt']:
+            encryt_credentials_list.append(k)
+    print  encryt_credentials_list
+
     if credentials is not None:
         # Get the credentials list from schema
-        encryt_credentials_list = ['vcenter_password', 'vsd_auth_password']
 
         for cred in encryt_credentials_list:
             if cred in credentials:
-                # encrypt passcode with password
                 secret = VaultSecret(passcode)
-                vault = VaultAES256.encrypt(credentials[cred], secret)
-                b_HEADER = b'$ANSIBLE_VAULT'
-                b_version = u'1.1'
-                b_cipher_name = u'AES256'
-                header_parts = [ b_HEADER, b_version, b_cipher_name]
-                header = b';'.join(header_parts)
-                b_vaulttext = [header]
-                b_vaulttext += [vault[i:i + 80] for i in range(0, len(vault), 80)]
-                b_vaulttext += [b'']
-                b_vaulttext = b'\n'.join(b_vaulttext)
-                
-                credentials[cred] = str(b_vaulttext)
-                #result = VaultAES256.decrypt(vault, secret)
-                #print result
+                editor = VaultEditor()
+                enc = editor.encrypt_bytes("hello", secret)
+                print str(enc)
+                encodedYaml = VaultYaml(enc)
+                credentials[cred] = encodedYaml
         print credentials
         with open(credentials_file, 'w') as file:
             yaml.dump(credentials, file, default_flow_style=False)
