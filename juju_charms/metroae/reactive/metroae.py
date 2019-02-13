f0feebd79d028b396a99a621569ae63fd157400a
Mike Piecuch <michael.piecuch@nokia.com>
2019-02-13 16:27:30 -0500

    Juju charm metroae layer

--- juju_charms/metroae/reactive/metroae.py
+++ juju_charms/metroae/reactive/metroae.py
@@ -1,12 +1,15 @@
 from charms.reactive import when, when_not, set_flag
 import subprocess
+import os
 
-METRO_DIRECTORY = './nuage-metro/'
+
+CHARM_DIR = os.environ['CHARM_DIR']
+METRO_DIRECTORY = os.path.join($CHARM_DIR, 'nuage-metro')
 
 
 @when_not('metroae.installed')
 def install_metroae():
-    run_shell("sudo virtualenv -p python2.7 metroae")
+    run_shell("virtualenv -p python2.7 metroae")
     #run_shell("./metro-setup.sh")
     set_flag("metroae.installed")
 
