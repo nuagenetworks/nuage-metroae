f29ee58e6adaa4f27669d376ee10356871d054a0
Mike Piecuch <michael.piecuch@nokia.com>
2019-02-15 16:41:16 -0500

    Juju charm for vsc deploy

--- juju_charms/metroae/reactive/metroae.py
+++ juju_charms/metroae/reactive/metroae.py
@@ -5,9 +5,12 @@ import os
 
 CHARM_DIR = os.environ['CHARM_DIR']
 METRO_DIR = os.path.join(CHARM_DIR, 'nuage-metro')
-IMAGE_URL = "http://135.227.146.142/packages/vsp/5.3.3/vsc.tar.gz"
-IMAGE_DIR = os.path.join(CHARM_DIR, 'images/vsc/single_disk')
-IMAGE_FULL_PATH = os.path.join(IMAGE_DIR, "vsc.tar.gz")
+VSD_IMAGE_URL = "http://135.227.146.142/packages/juju/VSD-5.3.3_99.qcow2"
+VSC_IMAGE_URL = "http://135.227.146.142/packages/juju/vsc_singledisk.qcow2"
+KEY_URL = "http://135.227.146.142/packages/juju/id_rsa"
+VSD_IMAGE_DIR = os.path.join(CHARM_DIR, 'images/vsd/qcow2/')
+VSC_IMAGE_DIR = os.path.join(CHARM_DIR, 'images/vsc/single_disk/')
+KEY_FILE = "/root/.ssh/id_rsa"
 
 
 @when_not('metroae.installed')
@@ -22,7 +25,6 @@ def install_metroae():
 def install_images():
     install_packages()
     pull_images()
-    unpack_images()
     create_deployment()
 
     set_flag("images.installed")
@@ -33,18 +35,18 @@ def install_packages():
 
 
 def pull_images():
-    if not os.path.exists(IMAGE_DIR):
-        os.makedirs(IMAGE_DIR)
+    if not os.path.exists(VSD_IMAGE_DIR):
+        os.makedirs(VSD_IMAGE_DIR)
 
-    run_shell("wget %s -O %s" % (IMAGE_URL, IMAGE_FULL_PATH))
+    run_shell("wget %s -O %s" % (VSD_IMAGE_URL, VSD_IMAGE_DIR))
 
+    if not os.path.exists(VSC_IMAGE_DIR):
+        os.makedirs(VSC_IMAGE_DIR)
 
-def unpack_images():
-    run_shell("/bin/gunzip " + IMAGE_FULL_PATH)
-    run_shell("/bin/tar -xf %s -C %s" % (IMAGE_FULL_PATH.rstrip(".gz"),
-                                         IMAGE_DIR))
-    os.rename(os.path.join(IMAGE_DIR, "vsc.qcow2"),
-              os.path.join(IMAGE_DIR, "vsc_singledisk.qcow2"))
+    run_shell("wget %s -O %s" % (VSC_IMAGE_URL, VSC_IMAGE_DIR))
+
+    run_shell("wget %s -O %s" % (KEY_URL, KEY_FILE))
+    os.chmod(KEY_FILE, 400)
 
 
 def create_deployment():
@@ -53,8 +55,17 @@ def create_deployment():
               os.path.join(METRO_DIR, "deployments/default"))
 
 
-@when_not('vsc.deployed')
+@when_not('vsd.deployed')
 @when('images.installed')
+def deploy_vsd():
+    run_shell("source .metroaenv/bin/activate && "
+              "HOME=/home/root ./metroae install_vsds "
+              "-vvv -e ansible_python_interpreter=python2.7")
+    set_flag("vsd.deployed")
+
+
+@when_not('vsc.deployed')
+@when('vsd.deployed')
 def deploy_vsc():
     run_shell("source .metroaenv/bin/activate && "
               "HOME=/home/root ./metroae install_vscs "
