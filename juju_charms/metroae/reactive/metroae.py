b641e2a1a71290f89a5116313ccdcb532079efec
Mike Piecuch <michael.piecuch@nokia.com>
2019-02-15 12:36:27 -0500

    Juju charm for vsc deploy

--- juju_charms/metroae/reactive/metroae.py
+++ juju_charms/metroae/reactive/metroae.py
@@ -4,7 +4,7 @@ import os
 
 
 CHARM_DIR = os.environ['CHARM_DIR']
-METRO_DIRECTORY = os.path.join(CHARM_DIR, 'nuage-metro')
+METRO_DIR = os.path.join(CHARM_DIR, 'nuage-metro')
 IMAGE_URL = "http://135.227.146.142/packages/vsp/5.3.3/vsc.tar.gz"
 IMAGE_DIR = os.path.join(CHARM_DIR, 'images/vsc/single_disk')
 IMAGE_FULL_PATH = os.path.join(IMAGE_DIR, "vsc.tar.gz")
@@ -23,6 +23,7 @@ def install_images():
     install_packages()
     pull_images()
     unpack_images()
+    create_deployment()
 
     set_flag("images.installed")
 
@@ -46,11 +47,18 @@ def unpack_images():
               os.path.join(IMAGE_DIR, "vsc_singledisk.qcow2"))
 
 
+def create_deployment():
+    run_shell("rm -rf " + os.path.join(METRO_DIR, "deployments/default"))
+    os.rename(os.path.join(CHARM_DIR, "deployment"),
+              os.path.join(METRO_DIR, "deployments/default"))
+
+
 @when_not('vsc.deployed')
 @when('images.installed')
 def deploy_vsc():
     run_shell("source .metroaenv/bin/activate && "
-              "HOME=/home/root ./metroae build -vvv -e ansible_python_interpreter=python2.7")
+              "HOME=/home/root ./metroae install_vscs "
+              "-vvv -e ansible_python_interpreter=python2.7")
     set_flag("vsc.deployed")
 
 
@@ -58,7 +66,7 @@ def run_shell(cmd):
     ret = subprocess.Popen(cmd,
                            shell=True,
                            executable='/bin/bash',
-                           cwd=METRO_DIRECTORY)
+                           cwd=METRO_DIR)
     retcode = ret.wait()
     if retcode != 0:
         raise Exception("Non-zero %d return code from %s" % (retcode, cmd))
