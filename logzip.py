d049b50a5e34ba05c11e31eaad2e15a9a0010061
Bhavya Dwivedi <bhavya.dwivedi.mnnit@gmail.com>
2019-11-14 14:21:25 -0500

    flak8 errors fixed

--- logzip.py
+++ logzip.py
@@ -4,30 +4,30 @@ from pathlib2 import Path
 import tarfile
 import argparse
 from time import gmtime, strftime
-now=strftime("%Y-%m-%d-%H-%M-%S",gmtime())
+now = strftime("%Y-%m-%d-%H-%M-%S" , gmtime())
 parser = argparse.ArgumentParser()
-parser.add_argument('--tarFileName', default='debug-'+now)
+parser.add_argument('--tarFileName', default='debug-' + now)
 parser.add_argument('--deploymentName')
 args = parser.parse_args()
-tarFileName=args.tarFileName
+tarFileName = args.tarFileName
 if args.deploymentName:
-	deploymentName="/deployments/"+args.deploymentName
+	deploymentName = "/deployments/" + args.deploymentName
 else:
-	deploymentName="/deployments/"
+	deploymentName = "/deployments/"
 
-cwdir=os.getcwd()
-ansibleLogPath=Path(cwdir+'/ansible.log')
-with tarfile.open( tarFileName + '.tar.gz', mode='w:gz') as archive:
+cwdir = os.getcwd()
+ansibleLogPath = Path(cwdir + '/ansible.log')
+with tarfile.open( tarFileName + '.tar.gz', mode = 'w:gz') as archive:
     try:
-        archive.add( cwdir + deploymentName, arcname='/unzipped/'+deploymentName,recursive=True)
+        archive.add( cwdir + deploymentName, arcname = '/unzipped/'+deploymentName,recursive=True)
     except:
         print("deployment directory doenst exits")
     try:
-        archive.add(cwdir+'/src/inventory', arcname='/unzipped/src/inventory',recursive=True)
+        archive.add(cwdir + '/src/inventory', arcname = '/unzipped/src/inventory',recursive=True)
     except:
         print("Inventory doesn't exist")
     if ansibleLogPath.exists():
-        archive.add(cwdir+'/ansible.log',arcname='/unzipped/ansible.log')
+        archive.add(cwdir + '/ansible.log',arcname = '/unzipped/ansible.log')
     else:
         print("ansible.log file doesn't exist")
     
