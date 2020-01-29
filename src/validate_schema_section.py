bbb5c2ae45e295f75c5b206423fbfecb99842c5e
Shuo Wang <leuction@gmail.com>
2020-01-29 14:14:35 -0500

    Fixed the format

--- src/validate_schema_section.py
+++ src/validate_schema_section.py
@@ -8,27 +8,40 @@ SECTION_BEGIN_FIELD = "sectionBegin"
 SECTION_END_FIELD = "sectionEnd"
 SCHEMA_DIRECTORY = "schemas"
 
+
 def validate_sections(schema_contents, file_name):
-    pattern = re.compile('"' + '(' + SECTION_BEGIN_FIELD + '|' + SECTION_END_FIELD + ')' + '":\\s*"(.*)"')
+    pattern = re.compile(
+      '"' + '(' + SECTION_BEGIN_FIELD + '|' + SECTION_END_FIELD + ')' + '":\\s*"(.*)"')
     it = pattern.finditer(schema_contents)
     stack = []
     for match in it:
-      if match.group(1) == SECTION_BEGIN_FIELD:
-        if len(stack) > 0:
-          # Two sectionBegin
-          print("Error in " + file_name + "! There are two overlap " + SECTION_BEGIN_FIELD + ': "' + stack.pop() + '" and "' + match.group(2) + '"')
-          exit(1)
-        stack.append(match.group(2))
-      elif match.group(1) == SECTION_END_FIELD:
-        if len(stack) == 0:
-          # No previous match
-          print("Error in " + file_name + "! There is no " + SECTION_BEGIN_FIELD + ' correspond to "' + SECTION_END_FIELD + '": ' + match.group(2))
-          exit(1)
-        pre_section = stack.pop()
-        if pre_section != match.group(2):
-          # Previous match is not the same as current section
-          print("Error in " + file_name + '! The previous ' + SECTION_BEGIN_FIELD + ': "' + pre_section + '" is not the same as "' + SECTION_END_FIELD + '": "' + match.group(2) + '"')
-          exit(1)
+        if match.group(1) == SECTION_BEGIN_FIELD:
+            if len(stack) > 0:
+                # Two sectionBegin
+                print(
+                  "Error in " + file_name + "! There are two overlap " +
+                  SECTION_BEGIN_FIELD + ': "' + stack.pop() + '" and "' +
+                  match.group(2) + '"')
+                exit(1)
+            stack.append(match.group(2))
+        elif match.group(1) == SECTION_END_FIELD:
+            if len(stack) == 0:
+                # No previous match
+                print(
+                  "Error in " + file_name + "! There is no " +
+                  SECTION_BEGIN_FIELD + ' correspond to "' +
+                  SECTION_END_FIELD + '": ' + match.group(2))
+                exit(1)
+            pre_section = stack.pop()
+            if pre_section != match.group(2):
+                # Previous match is not the same as current section
+                print(
+                  "Error in " + file_name + '! The previous ' +
+                  SECTION_BEGIN_FIELD + ': "' + pre_section +
+                  '" is not the same as "' + SECTION_END_FIELD + '": "' +
+                  match.group(2) + '"')
+                exit(1)
+
 
 def main():
 
