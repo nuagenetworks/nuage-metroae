f0e785f7df08a65c581ae24c232009da273c6c87
Mike Piecuch <michael.piecuch@nokia.com>
2020-01-29 16:05:23 -0500

    Add unresolved jinja2 checking for inventory

--- src/validate_schema_section.py
+++ src/validate_schema_section.py
@@ -11,7 +11,7 @@ SCHEMA_DIRECTORY = "schemas"
 
 def validate_sections(schema_contents, file_name):
     pattern = re.compile(
-      '"' + '(' + SECTION_BEGIN_FIELD + '|' + SECTION_END_FIELD + ')' + '":\\s*"(.*)"')
+        '"' + '(' + SECTION_BEGIN_FIELD + '|' + SECTION_END_FIELD + ')' + '":\\s*"(.*)"')
     it = pattern.finditer(schema_contents)
     stack = []
     for match in it:
@@ -19,27 +19,27 @@ def validate_sections(schema_contents, file_name):
             if len(stack) > 0:
                 # Two sectionBegin
                 print(
-                  "Error in " + file_name + "! There are two overlap " +
-                  SECTION_BEGIN_FIELD + ': "' + stack.pop() + '" and "' +
-                  match.group(2) + '"')
+                    "Error in " + file_name + "! There are two overlap " +
+                    SECTION_BEGIN_FIELD + ': "' + stack.pop() + '" and "' +
+                    match.group(2) + '"')
                 exit(1)
             stack.append(match.group(2))
         elif match.group(1) == SECTION_END_FIELD:
             if len(stack) == 0:
                 # No previous match
                 print(
-                  "Error in " + file_name + "! There is no " +
-                  SECTION_BEGIN_FIELD + ' correspond to "' +
-                  SECTION_END_FIELD + '": ' + match.group(2))
+                    "Error in " + file_name + "! There is no " +
+                    SECTION_BEGIN_FIELD + ' correspond to "' +
+                    SECTION_END_FIELD + '": ' + match.group(2))
                 exit(1)
             pre_section = stack.pop()
             if pre_section != match.group(2):
                 # Previous match is not the same as current section
                 print(
-                  "Error in " + file_name + '! The previous ' +
-                  SECTION_BEGIN_FIELD + ': "' + pre_section +
-                  '" is not the same as "' + SECTION_END_FIELD + '": "' +
-                  match.group(2) + '"')
+                    "Error in " + file_name + '! The previous ' +
+                    SECTION_BEGIN_FIELD + ': "' + pre_section +
+                    '" is not the same as "' + SECTION_END_FIELD + '": "' +
+                    match.group(2) + '"')
                 exit(1)
 
 
