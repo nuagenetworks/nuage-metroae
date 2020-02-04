14eaff8d3ef034dd8d2c5b47666ae02b383067d2
Shuo Wang <leuction@gmail.com>
2020-02-04 11:14:13 -0500

    reindent

--- src/validate_schema_section.py
+++ src/validate_schema_section.py
@@ -21,33 +21,33 @@ def validate_sections(schema_contents, file_name):
             if len(stack) > 0:
                 # Two sectionBegin
                 print(
-                  "Error in " + file_name +
-                  "! There are two overlap " +
-                  SECTION_BEGIN_FIELD + ': "' +
-                  stack.pop() + '" and "' +
-                  match.group(2) + '"')
+                    "Error in " + file_name +
+                    "! There are two overlap " +
+                    SECTION_BEGIN_FIELD + ': "' +
+                    stack.pop() + '" and "' +
+                    match.group(2) + '"')
                 exit(1)
             stack.append(match.group(2))
         elif match.group(1) == SECTION_END_FIELD:
             if len(stack) == 0:
                 # No previous match
                 print(
-                  "Error in " + file_name +
-                  "! There is no " + SECTION_BEGIN_FIELD +
-                  ' correspond to "' + SECTION_END_FIELD +
-                  '": ' + match.group(2))
+                    "Error in " + file_name +
+                    "! There is no " + SECTION_BEGIN_FIELD +
+                    ' correspond to "' + SECTION_END_FIELD +
+                    '": ' + match.group(2))
                 exit(1)
             pre_section = stack.pop()
             if pre_section != match.group(2):
                 # Previous match is not the same as current section
                 print(
-                  "Error in " + file_name +
-                  '! The previous ' +
-                  SECTION_BEGIN_FIELD +
-                  ': "' + pre_section +
-                  '" is not the same as "' +
-                  SECTION_END_FIELD + '": "' +
-                  match.group(2) + '"')
+                    "Error in " + file_name +
+                    '! The previous ' +
+                    SECTION_BEGIN_FIELD +
+                    ': "' + pre_section +
+                    '" is not the same as "' +
+                    SECTION_END_FIELD + '": "' +
+                    match.group(2) + '"')
                 exit(1)
 
 
