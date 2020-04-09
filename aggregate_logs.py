1f8adc396ab1fdeae9dacc3fbf9d23d19f4794aa
Mike Piecuch <michael.piecuch@nokia.com>
2020-04-09 12:46:08 -0400

    Log aggregation prototype

--- aggregate_logs.py
+++ aggregate_logs.py
@@ -2,7 +2,47 @@
 
 import argparse
 import datetime
+import dateutil.parser
 import os
+import re
+
+RE_MONTH_NAME = "(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)"
+RE_MONTH_NUM = "((1[0-2])|(0?[1-9]))"
+RE_DAY = "(([1-2][0-9])|(3[0-1]|(0?[1-9])))"
+RE_YEAR = "(20[0-9][0-9])"
+RE_HOUR = "((1[0-9])|(2[0-3])|(0?[0-9]))"
+RE_MIN = "([0-5][0-9])"
+RE_SEC = "([0-5][0-9])"
+RE_MSEC = "([0-9][0-9][[0-9])"
+
+DATE_PATTERNS = [
+    "({day}[ /-]{month_name}([ /-]{year})?)".format(year=RE_YEAR,
+                                                    month_name=RE_MONTH_NAME,
+                                                    day=RE_DAY),
+    "({month_name}([ ]+){day}([ ]+{year})?)".format(year=RE_YEAR,
+                                                    month_name=RE_MONTH_NAME,
+                                                    day=RE_DAY),
+    "({month}[/-]{day}[/-]{year})".format(year=RE_YEAR,
+                                          month=RE_MONTH_NUM,
+                                          day=RE_DAY),
+    "({year}[/-]{month}[/-]{day})".format(year=RE_YEAR,
+                                          month=RE_MONTH_NUM,
+                                          day=RE_DAY),
+    "({year}[/-]{month_name}[/-]{day})".format(year=RE_YEAR,
+                                               month_name=RE_MONTH_NAME,
+                                               day=RE_DAY)
+]
+
+date_patterns = list()
+
+TIME_PATTERNS = [
+    "({hour}:{min}:{sec}([,.]{msec})?)".format(hour=RE_HOUR,
+                                               min=RE_MIN,
+                                               sec=RE_SEC,
+                                               msec=RE_MSEC)
+]
+
+time_patterns = list()
 
 HTML_PAGE_HEADERS = """
 <html><head><title>Aggregated logs - %s</title></head><body>
@@ -435,16 +475,37 @@ def fatal_error(msg):
     exit(1)
 
 
+def complile_re_patterns():
+    for pattern in DATE_PATTERNS:
+        date_patterns.append(re.compile(pattern, re.IGNORECASE))
+
+    for pattern in TIME_PATTERNS:
+        time_patterns.append(re.compile(pattern, re.IGNORECASE))
+
+
 def aggregate_logs(args):
-    output("Searching for log files...")
+    output("Searching for log files ...")
 
     log_files = find_log_files(args.log_paths, args.log_extensions)
 
     output(str(len(log_files)) + " log files found")
 
-    output("Combining log files...")
+    log_list = read_log_files(log_files)
+    timestamps = list()
+    for i, log in enumerate(log_list):
+        # output("%d - %s" % (detect_prefix(log), log_files[i]))
+        # output(log[0].strip())
+        # output(" " * detect_prefix(log) + "^")
+        output("Detecting timestamps %d/%d ..." % (i + 1, len(log_list)))
+        timestamps.append(detect_timestamps(log))
+        # output(str(timestamps[i][0]))
 
-    write_aggregate_log(log_files, args.output_file)
+    output("Interleaving logs ...")
+    combined = interleave_logs(log_list, timestamps)
+
+    write_aggregate_log(log_files, combined, args.output_file)
+
+    output("\nDone! View in web browser: " + args.output_file)
 
 
 def find_log_files(paths, extensions):
@@ -467,36 +528,186 @@ def find_log_files(paths, extensions):
     return log_files
 
 
-def write_aggregate_log(log_files, output_file_name):
+def read_log_files(log_files):
+    log_list = list()
+    for file_name in log_files:
+        output("Reading %s ..." % file_name)
+        with open(file_name, "r") as f:
+            log_contents = f.read().decode("utf-8", "ignore")
+            log_list.append(log_contents.split("\n"))
+
+    return log_list
+
+
+def detect_prefix(log):
+    position_count = dict()
+
+    for line in log:
+        position = -1
+        words = line.strip().split(" ")
+        for word in words:
+            position += len(word) + 1
+            if position in position_count:
+                position_count[position] += 1
+            else:
+                position_count[position] = 1
+
+    threshold = int(len(log) * 0.75)
+
+    best_position = 0
+    for position, count in position_count.iteritems():
+        if count > threshold and position > best_position:
+            best_position = position
+
+    return best_position
+
+
+def detect_timestamps(log):
+    timestamps = list()
+
+    for line in log:
+        timestamps.append(find_timestamp_in_line(line))
+
+    return timestamps
+
+
+def find_timestamp_in_line(line):
+    date_string = find_pattern_match(date_patterns, line)
+    time_string = find_pattern_match(time_patterns, line)
+
+    if time_string is None:
+        return None
+
+    if date_string is None:
+        timestamp_str = time_string
+    else:
+        timestamp_str = " ".join([date_string, time_string])
+
+    try:
+        # return dateutil.parser.parse(timestamp_str)
+        return (dateutil.parser.parse(timestamp_str) -
+                datetime.datetime(1970, 1, 1)).total_seconds()
+    except Exception:
+        return None
+
+
+def find_pattern_match(patterns, line):
+    best_match = None
+    best_position = 999999999
+    for pattern in patterns:
+        match = pattern.search(line)
+        if match is not None and match.pos < best_position:
+            best_match = match.groups()[0]
+            best_position = match.pos
+
+    return best_match
+
+
+def interleave_logs(log_list, timestamps):
+    combined = list()
+    positions = initialize_positions(timestamps)
+
+    log_index = 0
+
+    # Interleave log lines with timestamps
+    while log_index is not None:
+        log_index = find_next_log_index(positions)
+        if log_index is not None:
+            add_log_lines(combined,
+                          positions[log_index],
+                          log_list[log_index],
+                          timestamps[log_index],
+                          log_index)
+
+    # Flush remaining lines
+    for log_index, log in enumerate(log_list):
+        add_log_lines(combined,
+                      positions[log_index],
+                      log,
+                      timestamps[log_index],
+                      log_index)
+
+    return combined
+
+
+def initialize_positions(timestamps):
+    positions = list()
+    for timestamp_set in timestamps:
+        first_timestamp = None
+        for timestamp in timestamp_set:
+            if timestamp is not None:
+                first_timestamp = timestamp
+                break
+
+        positions.append([first_timestamp, 0])
+
+    return positions
+
+
+def find_next_log_index(positions):
+    best_index = None
+    best_position = 999999999999.0
+    for i, position in enumerate(positions):
+        if position[0] is not None and position[0] < best_position:
+            best_index = i
+            best_position = position[0]
+
+    return best_index
+
+
+def add_log_lines(combined, position, log, timestamp_set, log_index):
+    first = True
+    while position[1] < len(log) and (
+            first or timestamp_set[position[1]] is None):
+        first = False
+        combined.append(log_index)
+        combined.append(log[position[1]])
+        position[1] += 1
+
+    if position[1] < len(timestamp_set):
+        position[0] = timestamp_set[position[1]]
+    else:
+        position[0] = None
+
+
+def write_aggregate_log(log_files, combined, output_file_name):
     with open(output_file_name, "w") as out_file:
         out_file.write(HTML_PAGE_HEADERS)
         out_file.write(HTML_PAGE_STYLE)
         out_file.write(HTML_PAGE_BODY)
         out_file.write(HTML_PAGE_SCRIPT_HEADERS)
 
-        out_file.write("lines = [\n'")
-        for file_name in log_files:
-            add_log_to_output_file(file_name, out_file)
-        out_file.write("'\n];\n")
+        out_file.write("lines = [\n")
+
+        for line in combined:
+            out_file.write(format_log_line(line))
+        # for file_name in log_files:
+        #     output("Adding %s ..." % file_name)
+        #     add_log_to_output_file(file_name, out_file)
+
+        out_file.write("''];\n")
 
         out_file.write(HTML_PAGE_SCRIPT_FOOTERS)
         out_file.write(HTML_PAGE_FOOTERS)
 
 
-def add_log_to_output_file(log_file_name, out_file):
-    with open(log_file_name, "r") as f:
-        log_contents = f.read().decode("utf-8", "ignore")
-        out_file.write(format_log_output(log_contents))
+# def add_log_to_output_file(log_file_name, out_file):
+#     with open(log_file_name, "r") as f:
+#         log_contents = f.read().decode("utf-8", "ignore")
+#         out_file.write(format_log_output(log_contents))
 
 
-def format_log_output(log_contents):
-    log_contents = log_contents.encode('ascii', 'xmlcharrefreplace')
-    log_contents = log_contents.replace("'", "&#39;")
-    log_contents = log_contents.replace("\\", "&#92;")
-    log_contents = log_contents.replace("<", "&lt;")
-    log_contents = log_contents.replace(">", "&gt;")
-    log_contents = log_contents.replace("\r", "")
-    return "',\n'".join(log_contents.split("\n"))
+def format_log_line(log_line):
+    if type(log_line) == int:
+        return "%3d, " % log_line
+    else:
+        log_line = log_line.encode('ascii', 'xmlcharrefreplace')
+        log_line = log_line.replace("'", "&#39;")
+        log_line = log_line.replace("\\", "&#92;")
+        log_line = log_line.replace("<", "&lt;")
+        log_line = log_line.replace(">", "&gt;")
+        log_line = log_line.replace("\r", "")
+        return "".join(["'", log_line, "',\n"])
 
 
 def main():
@@ -513,6 +724,8 @@ def main():
                         default="aggregated_logs.html")
 
     args = parser.parse_args()
+
+    complile_re_patterns()
 
     aggregate_logs(args)
 
