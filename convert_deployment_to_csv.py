fe80c8f00bbab1e6ca5e70fcf51f58fae4aa6ff0
Sanket <godbole.sanket@gmail.com>
2020-08-10 11:37:59 -0400

    Convert deployment to CSV

--- convert_deployment_to_csv.py
+++ convert_deployment_to_csv.py
@@ -6,6 +6,8 @@ import json
 
 
 SCHEMAS_DIRECTORY = "schemas"
+TABLE_MARGIN_TOP = 1
+TABLE_MARGIN_LEFT = 1
 
 
 def usage():
@@ -46,6 +48,45 @@ def writeCsvFile(filename, content):
     f.write(content)
 
 
+def escape_line(line):
+    if '"' in line or ',' in line:
+        return '"' + line.replace('"', '""') + '"'
+    return line
+
+
+def addContent(content):
+    csvContent = ''
+    for item in content:
+        if type(item) == dict:
+            csvContent += (write_table(item).encode("utf-8")) + '\n'
+        else:
+            csvContent += escape_line(item) + "," + '\n'
+    return csvContent
+
+
+def write_table(table):
+    schema_name = table["schema"]
+    schema = read_schema(schema_name)
+
+    lines = []
+
+    for i in range(TABLE_MARGIN_TOP):
+        lines.append("")
+
+    if "no_title" not in table:
+        lines.append(escape_line(schema["title"]))
+        lines.append(escape_line(schema["description"]))
+        lines.append("")
+
+    lines.append(",".join(table["headers"]))
+
+    # write_fields(lines, schema, table)
+
+    lines = [("," * TABLE_MARGIN_LEFT) + x for x in lines]
+
+    return ",\n".join(lines)
+
+
 def main():
 
     if len(sys.argv) != 3:
@@ -59,7 +100,25 @@ def main():
         getValuesFromDeployment(deployment_folder, deployment)
     print csv_file
     print read_schema('vsds')
-    writeCsvFile(csv_file, 'Sample content')
+    content = """
+
+    - MetroAE deployment template CSV
+    - ""
+    - This spreadsheet can be converted to a set of MetroAE deployment files for
+    - use in an install or upgrade.
+    - ""
+    - Fill out the fields of this spreadsheet, save as CSV and then issue "metroae
+    - wizard" or "convert_csv_to_deployment.py <csv_file> <deployment_name>"
+
+    - schema: common
+      headers: [Common, Values, "", Descriptions]
+      extra:
+        - access_bridge
+        - vsd_license_file
+    """
+    contentToWrite = addContent(yaml.safe_load(content))
+    print contentToWrite
+    writeCsvFile(csv_file, contentToWrite)
 
 
 if __name__ == '__main__':
