29be10883f96a8d5903b4f42d0ec92c3947d5a27
Sanket <godbole.sanket@gmail.com>
2020-08-10 10:32:20 -0400

    Convert deployment to csv

--- convert_deployment_to_csv.py
+++ convert_deployment_to_csv.py
@@ -1,6 +1,7 @@
 import sys
 from os import listdir
 from os.path import isfile, join, splitext
+import yaml
 
 
 def usage():
@@ -17,6 +18,11 @@ def getAllSchemas(deployment_folder):
     schemas = [splitext(f)[0] for f in listdir(deployment_folder)
                if isfile(join(deployment_folder, f))]
     print schemas
+    return schemas
+
+
+def getValuesFromDeployment(deployment_folder, deployment_name):
+    print yaml.safe_load(open(join(deployment_folder, deployment_name +'.yml')))
 
 
 def main():
@@ -27,7 +33,9 @@ def main():
 
     deployment_folder = sys.argv[1]
     csv_file = sys.argv[2]
-    getAllSchemas(deployment_folder)
+    schemas = getAllSchemas(deployment_folder)
+    for schema in schemas:
+        getValuesFromDeployment(deployment_folder, schema)
     print csv_file
 
 
