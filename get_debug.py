5ce8327fcc36ef32213a6ba5e7c046380c5870be
Rajpurohit <ashish.rajpurohit@nokia.com>
2020-09-17 02:04:52 -0400

    Changes in get_debug.py

--- get_debug.py
+++ get_debug.py
@@ -23,18 +23,18 @@ if run_mode == "repo":
     ansibleLogPath = os.path.join(cwdir, 'ansible.log')
     deploymentPath = os.path.join(cwdir, deploymentName)
     inventoryPath = os.path.join(cwdir, 'src/inventory')
-    print('The log path is:'+cwdir+'/metroae_data/'+tarFileName+".tar.gz")
+    print('The log path is:' + cwdir + '/metroae_data/' + tarFileName + ".tar.gz")
 else:
     ansibleLogPath = os.path.join(cwdir, 'nuage-metroae', 'ansible.log')
     deploymentPath = os.path.join(cwdir, 'nuage-metroae', deploymentName)
     inventoryPath = os.path.join(cwdir, 'nuage-metroae', 'src/inventory')
     tarFileName = os.path.join(cwdir, '/metroae_data', tarFileName)
-    print('The log path is:'+cwdir+'/metroae_data/'+tarFileName+".tar.gz")
+    print('The log path is:' + cwdir + '/metroae_data/' + tarFileName + ".tar.gz")
 with tarfile.open(tarFileName + '.tar.gz', mode='w:gz') as archive:
     try:
         archive.add(deploymentPath, arcname=os.path.join('/unzipped/', deploymentName), recursive=True)
     except:
-        print("Can't find"+deploymentName+"Skipping.")
+        print("Can't find" + deploymentName + "Skipping.")
     try:
         archive.add(inventoryPath, arcname='/unzipped/src/inventory', recursive=True)
     except:
@@ -43,15 +43,15 @@ with tarfile.open(tarFileName + '.tar.gz', mode='w:gz') as archive:
         archive.add(ansibleLogPath, arcname='/unzipped/ansible.log')
     else:
         print("Can't find ansible.log, Skipping.")
-command = "tar -tvf "+tarFileName+".tar.gz >files.txt"
+command = "tar -tvf " + tarFileName + ".tar.gz >files.txt"
 os.system(command)
-f=open("files.txt","r")
+f=open("files.txt", "r" )
 fread=f.read()
-outlist=re.findall("unzipped/.*/.*/(.*)",fread)
+outlist=re.findall("unzipped/.*/.*/(.*)", fread)
 for i in outlist:
     if(i==''):
         outlist.remove(i)
-print("The files included in the "+tarFileName+".tar.gz are:")
+print("The files included in the " + tarFileName + ".tar.gz are:")
 for i in outlist:
     if(i == ''):
         continue
