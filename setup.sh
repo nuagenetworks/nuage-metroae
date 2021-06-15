238b62ac5f1fb4b200d46e384126a525fe3fa44f
Urvish Rana <urvish.rana@nuagenetworks.net>
2021-06-15 11:16:23 -0600

    Used community collection

--- setup.sh
+++ setup.sh
@@ -289,7 +289,7 @@ function setup_tab_completion() {
 ##############################################################################
 function ansible_galaxy_install(){
   printn "Installing collections";
-  ansible-galaxy collection install -r collection_requirements.yml
+  ansible-galaxy collection install -r collection_requirements.yml -p ./collections/ansible_collections
   check_retcode $?
   chown caso:caso ansible.log
 
