3b49e81de7e441be4d3b07ea493de0dfbdad7f68
dahal <arogya.dahal@nokia.com>
2021-07-26 17:30:31 -0400

    Manually passed the vsd auth parameters

--- src/library/optional_password_vsd_gui.py
+++ src/library/optional_password_vsd_gui.py
@@ -29,10 +29,10 @@ options:
 EXAMPLES = '''
 - optional_password_vsd_gui:
     vsd_auth:
-        username: csproot
-        password: csproot
-        enterprise: csp
-        api_url: https://localhost:8443
+      username: csproot
+      password: csproot
+      enterprise: csp
+      api_url: https://localhost:8443
     vsd_version: 5.4.1
 '''
 
@@ -75,7 +75,10 @@ def main():
     )
 
     module = AnsibleModule(argument_spec=arg_spec, supports_check_mode=True)
-    vsd_auth = module.params['vsd_auth']
+    vsd_auth = {"username": "csproot",
+                "password": "csproot",
+                "enterprise": "csp",
+                "api_url": "https://vsd1.example.com:8443"}
     vsd_version = module.params['vsd_version']
 
     try:
