55fa2a6924531dac77aadfd45998def5489523be
vpatel4n <vandankumar.patel@nokia.com>
2021-11-29 09:47:15 -0500

    trial for snapshot

--- nesc-openstack-snapshot.sh
+++ nesc-openstack-snapshot.sh
@@ -6,10 +6,10 @@ status=$?
 if [[ $status -ne 0 ]]; then
         echo "Setting default MetroAE NESC OpenStack authencation failure."
         exit 1
-    else
-        echo "Successfully added authencation. Check it."
-        env | grep OS_
-    fi
+else
+    echo "Successfully added authencation. Check it."
+    env | grep OS_
+fi
 
 echo ""
 echo ""
@@ -19,8 +19,8 @@ echo ""
 echo "******************************************************END OF THE LIST*****************************************************"
 
 if [[ $status -ne 0 ]]; then
-        echo "Error to list down instanses from OpenStack"
-        exit 1
+    echo "Error to list down instanses from OpenStack"
+    exit 1
 fi
 echo "Check the list of instances and select."
 
@@ -30,8 +30,8 @@ openstack server stop myInstance
 status=$?
 
 if [[ $status -ne 0 ]]; then
-        echo "Failed to stop $nesc_instance. May be that instance is not valid."
-        exit 1
+    echo "Failed to stop $nesc_instance. May be that instance is not valid."
+    exit 1
 fi
 
 echo ""
@@ -42,75 +42,68 @@ echo ""
 echo "******************************************************END OF THE LIST*****************************************************"
 
 if [[ $status -ne 0 ]]; then
-        echo "Error to list down instanses from OpenStack"
-        exit 1
+    echo "Error to list down instanses from OpenStack"
+    exit 1
 fi
 
+read -p "Enter snapshot name:" nesc_instance_snap
 
+openstack server image create --name $nesc_instance_snap $nesc_instance
 
+status=$?
+if [[ $status -ne 0 ]]; then
+    echo "Error to create snapshot."
+    exit 1
+fi
 
+nova image-list
+status=$?
+if [[ $status -ne 0 ]]; then
+    echo "Error to list down images from OpenStack"
+    exit 1
+fi
 
+echo ""
+echo "*****DOWNLOAD SNAPSHOT*****"
 
+user_choice="init"
+while [[ $user_choice != "y" ]] && [[ $user_choice != "n" ]] && [[ $user_choice != "" ]]
+do
+    read -p "Do you want to download the snapshot?(y/n)" user_choice
+done
 
-
-
-else
-        debug "${FUNCNAME[0]}: Script log is not found"
-        sudo docker pull $METRO_AE_IMAGE:$MAX_CONTAINER_VERSION
-        status=$?
-    fi
-
+if [[ $user_choice -eq "y" ]]; then
+    read -p "Enter image ID of snapshot: " nesc_snapshot_id
+    openstack image save --file snapshot.raw $nesc_snapshot_id
+    
+    status=$?
     if [[ $status -ne 0 ]]; then
-        write_to_screen_and_script_log "Attempt to pull the $MAX_CONTAINER_VERSION MetroAE container image failed. Quitting."
+        echo "Error to download images from OpenStack"
         exit 1
-    else
-        write_to_screen_and_script_log "Successfully pulled the MetroAE container image from Docker hub"
     fi
-    set -e
+else
+    echo "You have to download snapshot image to import it into new environment."
+    echo ""
+fi
 
-    return $status
+openstack flavor list
+status=$?
+if [[ $status -ne 0 ]]; then
+    echo "Error to list down flavors from OpenStack"
+    exit 1
+fi
+read -p "Select snapshot flavor:" nesc_snapshot_flavor
 
-#1.default / user cred + error check msg
-#2. list
-#3. select instance
-#4. do process (make diff functions)
 
-#!/usr/bin/env bash
-# To use this file type this command "source nesc-openstack-snapshot.sh"
-# To use an OpenStack cloud you need to authenticate against the Identity
-# service named keystone, which returns a **Token** and **Service Catalog**.
-# The catalog contains the endpoints for all services the user/tenant has
-# access to - such as Compute, Image Service, Identity, Object Storage, Block
-# Storage, and Networking (code-named nova, glance, keystone, swift,
-# cinder, and neutron).
-#
-# *NOTE*: Using the 3 *Identity API* does not necessarily mean any other
-# OpenStack API is version 3. For example, your cloud provider may implement
-# Image API v1.1, Block Storage API v2, and Compute API v2.0. OS_AUTH_URL is
-# only for the Identity API served through keystone.
-export OS_AUTH_URL=http://ch-dc-os-gsn-32.eecloud.nsn-net.net:5000/v3
-# With the addition of Keystone we have standardized on the term **project**
-# as the entity that owns the resources.
-export OS_PROJECT_ID=f36f2db6bb434484b71a45aa84b9d790
-export OS_PROJECT_NAME="NI_NME_SERVICES_DELIVERYAUTOMATION_CLOUD"
-export OS_USER_DOMAIN_NAME="Default"
-if [ -z "$OS_USER_DOMAIN_NAME" ]; then unset OS_USER_DOMAIN_NAME; fi
-export OS_PROJECT_DOMAIN_ID="default"
-if [ -z "$OS_PROJECT_DOMAIN_ID" ]; then unset OS_PROJECT_DOMAIN_ID; fi
-# unset v2.0 items in case set
-unset OS_TENANT_ID
-unset OS_TENANT_NAME
-# In addition to the owning entity (tenant), OpenStack stores the entity
-# performing the action as the **user**.
-export OS_USERNAME="NI_NME_SERVICES_DELIVERYAUTOMATION_CLOUD"
-# With Keystone you pass the keystone password.
-# echo "Please enter your OpenStack Password for project $OS_PROJECT_NAME as user $OS_USERNAME: "
-# read -sr OS_PASSWORD_INPUT
-export OS_PASSWORD="casoCASO1$"
-# If your configuration has multiple regions, we set that information here.
-# OS_REGION_NAME is optional and only valid in certain environments.
-export OS_REGION_NAME="RegionOne"
-# Don't leave a blank variable, unset it if it was empty
-if [ -z "$OS_REGION_NAME" ]; then unset OS_REGION_NAME; fi
-export OS_INTERFACE=public
-export OS_IDENTITY_API_VERSION=3
+openstack network list
+status=$?
+if [[ $status -ne 0 ]]; then
+    echo "Error to list down network IDs from OpenStack"
+    exit 1
+fi
+read -p "Select network id:" nesc_openstack_net_id
+echo ""
+echo ""
+read -p "Enter new name for new instance: " nesc_snap_new_instance
+
+nova boot --flavor $nesc_snapshot_flavor --nic net-id=$nesc_openstack_net_id --image $nesc_instance_snap $nesc_snap_new_instance
