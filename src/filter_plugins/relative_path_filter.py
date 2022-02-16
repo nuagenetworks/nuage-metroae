1a58d4b00c90b8115a24a52d6d87e6a4ce368d1e
Eva <59952717+evamudgal@users.noreply.github.com>
2022-02-16 13:31:54 -0500

    Update relative_path_filter.py
    Debugging

--- src/filter_plugins/relative_path_filter.py
+++ src/filter_plugins/relative_path_filter.py
@@ -2,11 +2,14 @@ import os
 
 
 METROAE_ROOT = "/metroae/"
+print("INSIDE FILTER")
 
 
 def relative_path_to_absolute_path(string):
+    print("INSIDE FUNCTION")
     if not os.path.isabs(string):
         string = METROAE_ROOT + string.lstrip("./")
+        print("INSIDE CONDITION")
     return string
 
 
