59a316c51c10ef380f61b163045460efe5719af7
Eva <evamudgal@gmail.com>
2022-02-18 14:30:22 -0500

    Update relative_path_filter.py
    Remove print statements added to debug

--- src/filter_plugins/relative_path_filter.py
+++ src/filter_plugins/relative_path_filter.py
@@ -2,16 +2,11 @@ import os
 
 
 METROAE_ROOT = "/metroae/"
-print("INSIDE FILTER")
 
 
 def relative_path_to_absolute_path(string):
-    print("INSIDE FUNCTION")
     if not os.path.isabs(string):
-        print("PATH IS" + string)
         string = METROAE_ROOT + string.lstrip("./")
-        print("PATH IS" + string)
-        print("INSIDE CONDITION")
     return string
 
 
